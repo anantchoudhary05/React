48f5d78aef6cfa19f33959ecb04b2dd259851c9e
Your Name <choudharyanantkumar08@gmail.com>
2025-12-30 17:33:36 +0530

    igniting our app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Namste React
+
+
+# Parcel
+- Dev build
+- Local server
+- HMR (hot module replacement)
+- File watching Algorithm - written in c++
+- Caching - Faster Builds 
+- Image optimization
+- minification
+- bundling
+- Compressing
--- app.js
+++ app.js
@@ -1,17 +1,25 @@
-const heading = React.createElement("h1", { id: "heading" }, "Hello form react app");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
+
+  
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
 
 
 //react nested structure
 
 const parent = React.createElement("div", { id: "parent" },
-    [React.createElement("div", { id: "child1" },
-        [React.createElement("h1", {}, "I am h1 tag from child1"),
-        React.createElement("h2", {}, "I am h2 tag from child1")]
+    [React.createElement("div", { id: "child1",key:"child1" },
+        [React.createElement("h1", {id:"head1",key:"head1"}, "I am h1 tag from child1"),
+        React.createElement("h2", {id:"head2",key:"head2"}, "I am h2 tag from child1")]
     ),
-    React.createElement("div", { id: "child2" },
-        [React.createElement("h1", {}, "I am h1 tag from child2"),
-        React.createElement("h2", {}, "I am h2 tag from child2")])]
+    React.createElement("div", { id: "child2",key:"child2" },
+        [React.createElement("h1", {id:"head3",key:"head3"}, "I am h1 tag from child2"),
+        React.createElement("h2", {id:"head4",key:"head4"}, "I am h2 tag from child2")])]
 );
         //if we want to add more sibiling tag one after another to parent then we can do like this
 root.render(parent);
